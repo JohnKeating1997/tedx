547a523314bf2b6a868aa009b67574049cad6958
JohnKeating1997 <JohnKeatinghhh@gmail.com>
2021-04-08 08:36:36 +0800

    init

---
+++ src/views/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>TEDx</p>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .html{
+    color: black;
+  }
+</style>
